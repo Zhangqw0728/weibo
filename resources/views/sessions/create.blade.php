0ade5d33e05d13ff007b634111f1691a20a9d5cf
zhangqw <30414354+Zhangqw0728@users.noreply.github.com>
2021-03-22 11:00:38 +0000

    创建会话

---
+++ resources/views/sessions/create.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.default')
+@section('title', '登录')
+@section('content')
+    <div>
+        <h5>登录</h5>
+    </div>
+    <div>
+        @include('shared._errors')
+    </div>
+    <div>
+        <form method="POST" action="{{ route('login') }}">
+            {{ csrf_field() }}
+            <div>
+                <label for="email">邮箱</label>
+                <input type="text" name="email" value="{{ old('email') }}">
+            </div>
+            <div>
+                <label for="password">密码</label>
+                <input type="text" name="password" value="{{ old('password') }}">
+            </div>
+            <div>
+                <button type="submit">登录</button>
+            </div>
+        </form>
+    </div>
+@stop
