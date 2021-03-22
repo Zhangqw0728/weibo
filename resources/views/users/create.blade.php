2ae432e400a9673205d8e2b5ebf20c367461fe74
zhangqw <30414354+Zhangqw0728@users.noreply.github.com>
2021-03-22 09:04:03 +0000

    用户注册表单

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.default')
 @section('title', '注册')
 @section('content')
-    <h1>注册</h1>
+    <form method="POST" action="{{ route('users.store') }}">
+        <div>
+            <label for="name">名称</label>
+            <input type="text" name="name" value="{{ old('name') }}">
+        </div>
+        <div>
+            <label for="email">邮箱</label>
+            <input type="text" name="email" value="{{ old('email') }}">
+        </div>
+        <div>
+            <label for="password">密码</label>
+            <input type="text" name="password" value="{{ old('password') }}">
+        </div>
+        <div>
+            <label for="password_confirmation">确认密码</label>
+            <input type="text" name="password_confirmation" value="{{ old('password_confirmation') }}">
+        </div>
+        <button type="submit">注册</button>
+    </form>
 @stop
