2acac15178be3a20eb007afd36288dde87165421
zhangqw <30414354+Zhangqw0728@users.noreply.github.com>
2021-03-22 09:53:31 +0000

    完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,6 +4,9 @@
     <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
 </head>
 <body>
+@include('layouts._header')
+@include('shared._message')
 @yield('content')
+@include('layouts._footer')
 </body>
 </html>
---
+++ resources/views/shared/_message.blade.php
@@ -0,0 +1,9 @@
+@foreach(['danger', 'warning', 'success', 'info'] as $msg)
+    @if(session()->has($msg))
+        <div>
+            <p>
+                {{ session()->get($msg) }}
+            </p>
+        </div>
+    @endif
+@endforeach
