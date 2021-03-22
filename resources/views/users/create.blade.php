ceea0baf80797b8c08f1939709676e1a110de861
zhangqw <30414354+Zhangqw0728@users.noreply.github.com>
2021-03-22 09:39:23 +0000

    语言包提示

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.default')
 @section('title', '注册')
 @section('content')
+    @include('shared._errors')
     <form method="POST" action="{{ route('users.store') }}">
         {{ csrf_field() }}
         <div>
