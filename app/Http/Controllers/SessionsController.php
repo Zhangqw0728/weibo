3c227eb4049e4581efd53be85672f0bc4b91906f
zhangqw <30414354+Zhangqw0728@users.noreply.github.com>
2021-03-23 08:49:44 +0000

    用户激活

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -26,9 +26,15 @@ class SessionsController extends Controller
             'password' => 'required'
         ]);
         if (Auth::attempt($credentials, $request->has('remember'))) {
-            session()->flash('success', '欢迎回来');
-            $fallback = route('users.show', Auth::user());
-            return redirect()->intended($fallback);
+            if (Auth::user()->activated) {
+                session()->flash('success', '欢迎回来');
+                $fallback = route('users.show', Auth::user());
+                return redirect()->intended($fallback);
+            } else {
+                Auth::logout();
+                session()->flash('warning', '您的账号未激活，请检查邮箱并激活');
+                return redirect('/');
+            }
         } else {
             session()->flash('danger', '账号密码不匹配');
             return redirect()->back()->withInput();
---
+++ resources/views/emails/confirm.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>感谢您进行注册</h1>
+<p>
+    请点击下面的链接完成注册：
+    <a href="{{ route('confirm_email', $user->activation_token) }}">{{ route('confirm_email', $user->activation_token) }}</a>
+</p>
+</body>
+</html>
