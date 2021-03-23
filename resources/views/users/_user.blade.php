8bfa82e87c8d342aafa8e170ec85ff2dd7bbf450
zhangqw <30414354+Zhangqw0728@users.noreply.github.com>
2021-03-23 07:39:37 +0000

    删除管理员

--- resources/views/users/_user.blade.php
+++ resources/views/users/_user.blade.php
@@ -1,1 +1,10 @@
-<li><a href="{{ route('users.show', $user) }}">{{ $user->name }}</a></li>
+<li>
+    <a href="{{ route('users.show', $user) }}">{{ $user->name }}</a>
+    @can('destroy', $user)
+        <form action="{{ route('users.destroy', $user->id) }}" method="POST">
+            {{ csrf_field() }}
+            {{ method_field('DELETE') }}
+            <button type="submit">删除</button>
+        </form>
+    @endcan
+</li>
