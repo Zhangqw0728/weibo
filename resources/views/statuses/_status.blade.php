cf967db4acd617dbecc2eb773135a479a411259c
zhangqw <30414354+Zhangqw0728@users.noreply.github.com>
2021-03-24 12:00:21 +0000

    删除微博

--- resources/views/statuses/_status.blade.php
+++ resources/views/statuses/_status.blade.php
@@ -4,4 +4,12 @@
         <h5>{{ $status->created_at->diffForHumans() }}</h5>
         {{ $status->content }}
     </div>
+
+    @can('destroy', $status)
+        <form action="{{ route('statuses.destroy', $status->id) }}" method="post" onsubmit="return confirm('确定删除？')">
+            {{ csrf_field() }}
+            {{ method_field('DELETE') }}
+            <button type="submit">删除</button>
+        </form>
+    @endcan
 </li>
